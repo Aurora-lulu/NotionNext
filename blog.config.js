1aa56049e33ec0be0a198249b7abb230167b3aed
baishan <2067140742@qq.com>
2023-07-01 13:38:12 +0800

    Update

--- blog.config.js
+++ blog.config.js
@@ -141,13 +141,7 @@ const BLOG = {
   // 鼠标点击烟花特效
   FIREWORKS: process.env.NEXT_PUBLIC_FIREWORKS || true, // 开关
   // 烟花色彩，感谢 https://github.com/Vixcity 提交的色彩
-  FIREWORKS_COLOR: [
-    '255, 20, 97',
-    '24, 255, 146',
-    '0,0,205',
-    '251,243, 140',
-    '255,182,193'
-  ],
+  FIREWORKS_COLOR: ['255, 20, 97', '24, 255, 146', '0,0,205', '255,182,193'],
 
   // 樱花飘落特效
   SAKURA: process.env.NEXT_PUBLIC_SAKURA || true, // 开关
